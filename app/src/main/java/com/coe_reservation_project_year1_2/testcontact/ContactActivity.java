00763303824ed44d5e0e15784f9c68e802c6a102
wuttiamornsak1999 <pornsukon@kkumail.com>
2018-05-13 01:00:40 +0700

    Initial commit

---
+++ app/src/main/java/com/coe_reservation_project_year1_2/testcontact/ContactActivity.java
@@ -0,0 +1,13 @@
+package com.coe_reservation_project_year1_2.testcontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
